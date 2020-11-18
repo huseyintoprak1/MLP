7f490f28ba7be6239cfe396a26c5b846c0400188
huseyintoprak1 <74530496+huseyintoprak1@users.noreply.github.com>
2020-11-18 13:41:54 +0300

    Create grid.py

---
+++ grid.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+myLabel1 = Label(root, text = "Hello World!").grid(row=0, column=0)
+myLabel2 = Label(root, text = "Selamlar").grid(row=1, column=1)
+
+#myLabel1.grid(row=0, column=0)
+#myLabel2.grid(row=1, column=1)
+root.mainloop()
