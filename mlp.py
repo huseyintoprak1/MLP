7044dc33a5ac81291c6bf7b5a4ecf1bc95b2438d
huseyintoprak1 <huseyintoprak1@gmail.com>
2020-12-01 21:21:33 +0300

    start
    start of project

---
+++ mlp.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+root.iconbitmap('C:\\Users\\Toprak\\Desktop\\MLP\\ytulogo.ico')
+root.title('Yapay Zeka Arayüzü')
+root.geometry("400x400")
+
+
+
+
+
+
+
+
+root.mainloop()
