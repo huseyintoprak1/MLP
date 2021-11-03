74fc1c3ff295ad7f5f37aeafe0789cef54c06db9
huseyintoprak1 <74530496+huseyintoprak1@users.noreply.github.com>
2021-11-03 09:34:08 +0300

    Delete grid.py

--- grid.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-
-root = Tk()
-myLabel1 = Label(root, text = "Hello World!").grid(row=0, column=0)
-myLabel2 = Label(root, text = "Selamlar").grid(row=1, column=1)
-
-#myLabel1.grid(row=0, column=0)
-#myLabel2.grid(row=1, column=1)
-root.mainloop()
